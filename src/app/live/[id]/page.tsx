6e30a2a5a096c81c858049f64c534f14177cc756
antnf3 <antnf3@gmail.com>
2023-07-21 11:25:05 +0900

    add stream create

--- src/app/live/[id]/page.tsx
+++ src/app/live/[id]/page.tsx
@@ -2,9 +2,19 @@ const LiveDetail = () => {
   return (
     <div className="space-y-4 px-4 py-10">
       <div className="aspect-video w-full rounded-md bg-slate-300 shadow-sm"></div>
-      <h3 className="mt-2 text-xl font-semibold text-gray-800">
-        Let&apos;s try potatos
-      </h3>
+      <h3 className="mt-2 text-xl font-semibold text-gray-800">Galaxy S50</h3>
+      <p className="mt-3 block text-2xl text-gray-900">$140</p>
+      <p className="my-6 text-base text-gray-600">
+        My money&apos;s in that office, right? If she start giving me some
+        bullshit about it ain&apos;t there, and we got to go someplace else and
+        get it, I&apos;m gonna shoot you in the head then and there. Then
+        I&apos;m gonna shoot that bitch in the kneecaps, find out where my
+        goddamn money is. She gonna tell me too. Hey, look at me when I&apos;m
+        talking to you, motherfucker. You listen: we go in there, and that ni**a
+        Winston or anybody else is in there, you the first motherfucker to get
+        shot. You understand?
+      </p>
+
       <div className="mt-10 h-[50vh] space-y-4 overflow-y-scroll px-4 py-10 pb-16">
         <div className="flex items-start space-x-2">
           <div className="h-8 w-8 rounded-full bg-slate-300" />
--- src/app/live/create/page.tsx
+++ src/app/live/create/page.tsx
@@ -1,29 +1,6 @@
-const Upload = () => {
+const Create = () => {
   return (
-    <div className="space-y-5 px-4 py-16">
-      <div>
-        <label
-          htmlFor="price"
-          className="flex h-48 w-full cursor-pointer items-center justify-center rounded-md border-2 border-dashed border-gray-300 text-gray-600 transition-colors hover:border-orange-400 hover:text-orange-400"
-        >
-          <svg
-            className="h-12 w-12"
-            stroke="currentColor"
-            fill="none"
-            viewBox="0 0 48 48"
-            aria-hidden="true"
-          >
-            <path
-              d="M28 8H12a4 4 0 00-4 4v20m32-12v8m0 0v8a4 4 0 01-4 4H12a4 4 0 01-4-4v-4m32-4l-3.172-3.172a4 4 0 00-5.656 0L28 28M8 32l9.172-9.172a4 4 0 015.656 0L28 28m0 0l4 4m4-24h8m-4-4v8m-12 4h.02"
-              strokeWidth={2}
-              strokeLinecap="round"
-              strokeLinejoin="round"
-            />
-          </svg>
-
-          <input id="price" className="hidden" type="file" />
-        </label>
-      </div>
+    <div className="space-y-5 px-4 py-10">
       <div>
         <label className="mb-1 block  text-sm font-medium text-gray-800">
           Name
@@ -31,8 +8,7 @@ const Upload = () => {
         <div className="relative flex items-center rounded-md shadow-sm">
           <input
             type="text"
-            placeholder="name"
-            className="w-full appearance-none rounded-md border border-gray-300 py-2  placeholder-gray-400 shadow-sm transition-colors focus:border-orange-500 focus:outline-none focus:ring-orange-500"
+            className="w-full appearance-none rounded-md border border-gray-300 py-2 placeholder-gray-400 shadow-sm transition-colors focus:border-orange-500 focus:outline-none focus:ring-orange-500"
           />
         </div>
       </div>
@@ -64,10 +40,9 @@ const Upload = () => {
         />
       </div>
       <button className="mt-5 w-full rounded-md border border-transparent bg-orange-500 px-3 py-2 text-sm font-medium text-white shadow-sm transition-colors hover:bg-orange-600 focus:ring-2 focus:ring-orange-500 focus:ring-offset-2">
-        Upload product
+        Go live
       </button>
     </div>
   );
 };
-
-export default Upload;
+export default Create;
